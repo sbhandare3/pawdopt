d2511b753822870be6549c22f8fc9b67c9a6e6bb
sbhandare3 <bhandare.shreyas@gmail.com>
2019-09-11 23:18:22 -0400

    first commit

---
+++ src/main/java/com/sbhandare/pawdopt/PawdoptApplication.java
@@ -0,0 +1,13 @@
+package com.sbhandare.pawdopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawdoptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawdoptApplication.class, args);
+	}
+
+}
