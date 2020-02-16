d8ccf1afc5b68dea5d56c6522c2241f6cb621445
Shreyas Bhandare <ssb146@scarletmail.rutgers.edu>
2020-02-16 15:48:18 -0500

    swagger config auth working

--- src/main/java/com/sbhandare/pawdopt/Config/SwaggerConfig.java
+++ src/main/java/com/sbhandare/pawdopt/Config/SwaggerConfig.java
@@ -3,13 +3,16 @@ package com.sbhandare.pawdopt.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Created by shreyas on 2/15/2020.
@@ -17,12 +20,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build().apiInfo(apiEndPointsInfo());
+                .build().apiInfo(apiEndPointsInfo())
+                .globalOperationParameters(singletonList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(true)
+                                .hidden(true)
+                                .defaultValue("Bearer ")
+                                .build()
+                        )
+                );
     }
 
     private ApiInfo apiEndPointsInfo() {
@@ -34,4 +49,5 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .build();
     }
+
 }
