5180eed263d35b77a988bf1b00057139e225d189
Shreyas Bhandare <ssb146@scarletmail.rutgers.edu>
2020-03-19 17:59:36 -0400

    grant type refresh token - internal server error resolved

--- src/main/java/com/sbhandare/pawdopt/Config/AuthorizationServerConfig.java
+++ src/main/java/com/sbhandare/pawdopt/Config/AuthorizationServerConfig.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    private TokenStore tokenStore = new InMemoryTokenStore();
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Autowired
     private AuthenticationManager authenticationManager;
     @Value("${security.oauth2.client.client-id}")
@@ -30,7 +36,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
-                .authenticationManager(authenticationManager);
+                .tokenStore(this.tokenStore)
+                .authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+
     }
 
     /**
