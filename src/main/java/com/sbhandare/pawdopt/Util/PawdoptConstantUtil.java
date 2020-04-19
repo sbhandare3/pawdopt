c77597126191b84cc65bd77b1edf9ee3ec5e421b
Shreyas Bhandare <ssb146@scarletmail.rutgers.edu>
2020-04-19 18:35:09 -0400

    url changed for making auto login request to server

--- src/main/java/com/sbhandare/pawdopt/Util/PawdoptConstantUtil.java
+++ src/main/java/com/sbhandare/pawdopt/Util/PawdoptConstantUtil.java
@@ -7,7 +7,7 @@ public class PawdoptConstantUtil {
     public static final int DEFAULT_PAGE_RESULTS = 10;
     public static final String ROLE_USER = "ROLE_USER";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
-    public static final String OAUTH_TOKEN_URL = "http://localhost:8080/api/v1/oauth/token";
+    public static final String OAUTH_TOKEN_URL = "https://pawdopt.herokuapp.com/api/v1/oauth/token";
     public static final String AUTO_LOGIN_FAILED = "Failed to log in. Please try again.";
     public static final String PET_BREED_PRIMARY = "primary";
     public static final String PET_BREED_SECONDARY = "secondary";
